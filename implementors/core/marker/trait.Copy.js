7c59afa75fc6676cc5ea75d9539aa46a9778e868
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-05 14:34:56 +0000

    Deploy tower-rs/tower to github.com/tower-rs/tower.git:gh-pages

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["tower_balance"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"tower_balance/choose/struct.TooFew.html\" title=\"struct tower_balance::choose::TooFew\">TooFew</a>",synthetic:false,types:["tower_balance::choose::TooFew"]},];
+implementors["tower_rate_limit"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"tower_rate_limit/struct.Rate.html\" title=\"struct tower_rate_limit::Rate\">Rate</a>",synthetic:false,types:["tower_rate_limit::Rate"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/tower_discover/trait.Discover.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["tower_balance"] = [{text:"impl&lt;D:&nbsp;<a class=\"trait\" href=\"tower_discover/trait.Discover.html\" title=\"trait tower_discover::Discover\">Discover</a>, M:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a>&gt; <a class=\"trait\" href=\"tower_discover/trait.Discover.html\" title=\"trait tower_discover::Discover\">Discover</a> for <a class=\"struct\" href=\"tower_balance/load/struct.Constant.html\" title=\"struct tower_balance::load::Constant\">Constant</a>&lt;D, M&gt;",synthetic:false,types:["tower_balance::load::constant::Constant"]},{text:"impl&lt;D:&nbsp;<a class=\"trait\" href=\"tower_discover/trait.Discover.html\" title=\"trait tower_discover::Discover\">Discover</a>&gt; <a class=\"trait\" href=\"tower_discover/trait.Discover.html\" title=\"trait tower_discover::Discover\">Discover</a> for <a class=\"struct\" href=\"tower_balance/load/struct.WithPendingRequests.html\" title=\"struct tower_balance::load::WithPendingRequests\">WithPendingRequests</a>&lt;D&gt;",synthetic:false,types:["tower_balance::load::pending_requests::WithPendingRequests"]},];
+implementors["tower_discover"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
